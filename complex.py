e57e9ba4259943d18270c8bff23fde13576bc555
amirforus <amir.yeganehk83@gmail.com>
2024-03-04 00:30:54 +0330

    maybe first version

---
+++ complex.py
@@ -0,0 +1,14 @@
+import cmath 
+
+# z = 1+1j
+
+z = complex(input ("Enter the equation :? "))
+
+# result = z ** 10
+power = int(input("Enter the power value :?"))
+
+result = z ** power
+
+print(result)
+
+print("---------------------------amir yeganeh---------------------------")
